e188d66f0cb8979391cb14b9a78998cc66fc0c32
Jdoink <140544769+Jdoink@users.noreply.github.com>
2025-03-15 18:25:30 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import WalletConnectButton from '@/components/WalletConnectButton';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>The Big Game - Betting DApp</h1>
+      <WalletConnectButton />
+      <p>Coming soon: Vegas-themed UI and betting integration!</p>
+    </div>
+  );
+}
