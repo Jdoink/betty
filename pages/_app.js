ded8f6b4a92551efbec5aa731713b3bae8b6393c
Jdoink <140544769+Jdoink@users.noreply.github.com>
2025-03-15 18:24:47 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+mport '@/styles/globals.css';
+import { WagmiConfig, configureChains, createConfig } from 'wagmi';
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { optimism, arbitrum, base } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains([optimism, arbitrum, base], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Big Game DApp',
+  projectId: '3fbf04dd4e08e90c396c75810f9bb71e',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
